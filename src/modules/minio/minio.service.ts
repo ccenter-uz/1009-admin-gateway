13869da7558527858cc9f78f65f040bf9a3c415b
imronshoimov <imronshoimov125@gmail.com>
2025-05-28 11:28:25 +0500

    change

--- src/modules/minio/minio.service.ts
+++ src/modules/minio/minio.service.ts
@@ -36,7 +36,7 @@ export class MinioService implements OnModuleInit {
 
   async uploadFiles(
     files: Array<Multer.File>,
-    bucketName: string = 'test'
+    bucketName = MinioConfig.bucketName
   ): Promise<{ link: string }[]> {
     const uploadedLinks: { link: string }[] = [];
 
